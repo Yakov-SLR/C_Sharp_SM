0d9caaae86d81a2bd1f76e0a5ccdf1ee37310a00
Yakov Egortsev <yakov@egortsev.ru>
2023-01-27 00:36:57 +0300

    Preapering

--- DZ002_Sravnenie_2_chisel_EgortsevYS/Program.cs
+++ DZ002_Sravnenie_2_chisel_EgortsevYS/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает 
+//два числа и выдает, какое число большее, а какое меньшее.
+Console.Clear();
+Console.Write("Сравнение двух чисел");
+Console.Write("Введите число № 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число № 2: ");
+int number2 = int.Parse(Console.ReadLine());
+if(number1 < number2) {
+    Console.WriteLine($"Число № 2 больше и составляет: {number2}");
+    Console.WriteLine($"Число № 1 меньше и составляет: {number1}");
+    return;
+}
+if (number2 < number1) {
+    Console.WriteLine($"Число № 1 больше и составляет: {number1} ");
+    Console.WriteLine($"Число № 2 меньше и составляет: {number2} ");
+    return;
+}
+if(number1 == number2) {
+    Console.WriteLine($"Числа № 1 и № 2 равны и составляют: {number1} и {number2}");
+    return;
+}
