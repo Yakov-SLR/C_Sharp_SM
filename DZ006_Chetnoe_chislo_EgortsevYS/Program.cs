7f1da60c14a88d654fad453f1634891d99f6c92b
Yakov Egortsev <yakov@egortsev.ru>
2023-01-28 23:45:56 +0300

    For check

---
+++ DZ006_Chetnoe_chislo_EgortsevYS/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+Console.Clear();
+Console.WriteLine("Проверка чётности числа");
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if(number % 2 == 0) {
+    Console.WriteLine($"Введённое число {number} является чётным");
+}
+else {
+    Console.WriteLine($"Введённое число {number} является нечётным");
+}
