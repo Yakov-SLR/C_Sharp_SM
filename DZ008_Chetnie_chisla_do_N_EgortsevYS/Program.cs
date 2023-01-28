8f46ed260d33fb1c93dac95e1e911081a295dfe1
Yakov Egortsev <yakov@egortsev.ru>
2023-01-29 00:13:12 +0300

    For Check

---
+++ DZ008_Chetnie_chisla_do_N_EgortsevYS/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+Console.Clear();
+Console.WriteLine("Вывод чётных чисел до числа N");
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int chetnoe = 1;
+Console.WriteLine("Выводим чётные числа: ");
+while (chetnoe < N) 
+{
+if(chetnoe % 2 == 0)
+ {
+Console.Write(" ");
+Console.Write(chetnoe);
+ }
+ chetnoe = chetnoe + 1;
+}
