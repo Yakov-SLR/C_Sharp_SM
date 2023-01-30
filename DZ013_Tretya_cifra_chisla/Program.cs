fdd876e7e0a056dad2774aca55284aff0e12b684
Yakov Egortsev <yakov@egortsev.ru>
2023-01-31 02:52:29 +0300

    Zadacha № 13

---
+++ DZ013_Tretya_cifra_chisla/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.WriteLine("Отображение третьей цифры заданного числа");
+Console.WriteLine("Введите целое число: ");
+int num = int.Parse(Console.ReadLine());
+if(num < 100)
+{
+    Console.WriteLine("Введите корректное число (трёхзначное и более)!");
+}
+else
+{
+    
+    string str = num.ToString();
+    string num3 = Convert.ToString(str[2]);
+    Console.WriteLine($"Третья цифра равна: {num3}");
+}
