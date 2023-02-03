b9e4b47f14b7424693c8130a9264a14a9d61418f
Yakov Egortsev <yakov@egortsev.ru>
2023-02-03 03:18:50 +0300

    For check

---
+++ DZ019_Palindrom/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+Console.WriteLine("Проверка числа на палиндром");
+Console.WriteLine("Введите пятизначное число: ");
+int chislo = int.Parse(Console.ReadLine());
+if(chislo < 0)
+{
+    otr = chislo;
+    chislo = chislo * -1;
+}
+if(chislo <= 9999 || chislo > 99999) 
+{
+    Console.WriteLine("Введите корректное пятизначное число");
+    return;
+}
+else
+{
+    Console.WriteLine("Число корректно, продолжаем.");
+} 
+string str = chislo.ToString();
+if(str[0] == str[4] && str[1] == str[3])
+{
+    Console.WriteLine($"Число {chislo} является палиндромом.");
+    Console.WriteLine("Благодарим за использование программы!");
+}
+else
+{
+    Console.WriteLine($"Число {chislo} не является палиндромом.");
+    Console.WriteLine("Благодарим за использование программы!");
+}
